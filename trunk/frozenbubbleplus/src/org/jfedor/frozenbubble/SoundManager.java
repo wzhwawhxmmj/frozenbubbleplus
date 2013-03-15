79e8f1a40f583bdc2c368ae71c655ad501246607
forteri76@gmail.com <forteri76@gmail.com@d79bdc4b-2ab0-6828-90ec-10c804b1398a>
2013-03-15 15:57:16 +0000

    Cleaned up SoundManager.java code.  Added cleanUp() to FrozenBubble.java.  Added method to SplashScreen.java to check whether to display the splash screen.

--- trunk/frozenbubbleplus/src/org/jfedor/frozenbubble/SoundManager.java
+++ trunk/frozenbubbleplus/src/org/jfedor/frozenbubble/SoundManager.java
@@ -63,39 +63,49 @@ public class SoundManager
   private int[] sm;
   Context context;
 
-  public SoundManager(Context context) {
+  public SoundManager( Context context )
+  {
     this.context = context;
-    soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
-    sm = new int[FrozenBubble.NUM_SOUNDS];
-    sm[FrozenBubble.SOUND_WON] = soundPool.load(context, R.raw.applause, 1);
-    sm[FrozenBubble.SOUND_LOST] = soundPool.load(context, R.raw.lose, 1);
-    sm[FrozenBubble.SOUND_LAUNCH] = soundPool.load(context, R.raw.launch, 1);
-    sm[FrozenBubble.SOUND_DESTROY] =
-        soundPool.load(context, R.raw.destroy_group, 1);
-    sm[FrozenBubble.SOUND_REBOUND] =
-        soundPool.load(context, R.raw.rebound, 1);
-    sm[FrozenBubble.SOUND_STICK] = soundPool.load(context, R.raw.stick, 1);
-    sm[FrozenBubble.SOUND_HURRY] = soundPool.load(context, R.raw.hurry, 1);
-    sm[FrozenBubble.SOUND_NEWROOT] =
-        soundPool.load(context, R.raw.newroot_solo, 1);
-    sm[FrozenBubble.SOUND_NOH] = soundPool.load(context, R.raw.noh, 1);
+    soundPool    = new SoundPool( 4, AudioManager.STREAM_MUSIC, 0 );
+    sm           = new int[ FrozenBubble.NUM_SOUNDS ];
+
+    sm[ FrozenBubble.SOUND_WON     ] =
+      soundPool.load( context, R.raw.applause,      1 );
+    sm[ FrozenBubble.SOUND_LOST    ] =
+      soundPool.load( context, R.raw.lose,          1 );
+    sm[ FrozenBubble.SOUND_LAUNCH  ] =
+      soundPool.load( context, R.raw.launch,        1 );
+    sm[ FrozenBubble.SOUND_DESTROY ] =
+      soundPool.load( context, R.raw.destroy_group, 1 );
+    sm[ FrozenBubble.SOUND_REBOUND ] =
+      soundPool.load( context, R.raw.rebound,       1 );
+    sm[ FrozenBubble.SOUND_STICK   ] =
+      soundPool.load( context, R.raw.stick,         1 );
+    sm[ FrozenBubble.SOUND_HURRY   ] =
+      soundPool.load( context, R.raw.hurry,         1 );
+    sm[ FrozenBubble.SOUND_NEWROOT ] =
+      soundPool.load( context, R.raw.newroot_solo,  1 );
+    sm[ FrozenBubble.SOUND_NOH     ] =
+      soundPool.load( context, R.raw.noh,           1 );
   }
 
-  public final void playSound(int sound) {
-    if (FrozenBubble.getSoundOn()) {
-      AudioManager mgr = (AudioManager)context.getSystemService(
-          Context.AUDIO_SERVICE);  
-      float streamVolumeCurrent =
-          mgr.getStreamVolume(AudioManager.STREAM_MUSIC);
-      float streamVolumeMax = mgr.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-      float volume = streamVolumeCurrent / streamVolumeMax;
-      soundPool.play(sm[sound], volume, volume, 1, 0, 1f);
+  public final void playSound( int sound )
+  {
+    if ( FrozenBubble.getSoundOn() )
+    {
+      AudioManager mgr =
+        (AudioManager)context.getSystemService( Context.AUDIO_SERVICE );
+      float volume     = mgr.getStreamVolume   ( AudioManager.STREAM_MUSIC );
+      float volumeMax  = mgr.getStreamMaxVolume( AudioManager.STREAM_MUSIC );
+      volume           = volume / volumeMax;
+      soundPool.play( sm[ sound ], volume, volume, 1, 0, 1f );
     }
   }
 
-  public final void cleanUp() {
-    sm = null;
-    context = null;
+  public final void cleanUp()
+  {
+    sm        = null;
+    context   = null;
     soundPool.release();
     soundPool = null;
   }
