cc1fd0a42884090c190e3958d62566708edf7850
forteri76@gmail.com <forteri76@gmail.com@d79bdc4b-2ab0-6828-90ec-10c804b1398a>
2014-05-19 15:58:54 +0000

    Merging latest changes from trunk.

--- branches/ouya/frozenbubbleplus/src/org/jfedor/frozenbubble/GameView.java
+++ branches/ouya/frozenbubbleplus/src/org/jfedor/frozenbubble/GameView.java
@@ -1006,9 +1006,9 @@ public class GameView extends SurfaceView
          * the pointers to null to facilitate garbage collection.  So I did
          * and the crashes went away.
          */
+        mImagesReady = false;
         mFrozenGame1 = null;
         mFrozenGame2 = null;
-        mImagesReady = false;
 
         boolean imagesScaled = (mBackgroundOrig == mBackground.bmp);
         mBackgroundOrig.recycle();
@@ -1199,7 +1199,7 @@ public class GameView extends SurfaceView
 
     private void doDraw(Canvas canvas) {
       //Log.i("frozen-bubble", "doDraw()");
-      if (! mImagesReady) {
+      if (!mImagesReady) {
         //Log.i("frozen-bubble", "!mImagesReady, returning");
         return;
       }
@@ -1208,9 +1208,13 @@ public class GameView extends SurfaceView
         canvas.drawRGB(0, 0, 0);
       }
       drawBackground(canvas);
-      mFrozenGame1.paint(canvas, mDisplayScale, mPlayer1DX, mDisplayDY);
+      if (mFrozenGame1 != null) {
+        mFrozenGame1.paint(canvas, mDisplayScale, mPlayer1DX, mDisplayDY);
+      }
       if (numPlayers > 1) {
-        mFrozenGame2.paint(canvas, mDisplayScale, mPlayer2DX, mDisplayDY);
+        if (mFrozenGame2 != null) {
+          mFrozenGame2.paint(canvas, mDisplayScale, mPlayer2DX, mDisplayDY);
+        }
         drawWinTotals(canvas);
       }
       else {
@@ -1833,8 +1837,8 @@ public class GameView extends SurfaceView
           mLevelManager.goToFirstLevel();
         }
 
+        mImagesReady = false;
         mPlayer1.setGameRef(null);
-        mFrozenGame1 = null;
         mFrozenGame1 = new FrozenGame(mBackground, mBubbles, mBubblesBlind,
                                       mFrozenBubbles, mTargetedBubbles,
                                       mBubbleBlink, mGameWon, mGameLost,
@@ -1849,7 +1853,6 @@ public class GameView extends SurfaceView
 
         if (numPlayers > 1) {
           mPlayer2.setGameRef(null);
-          mFrozenGame2 = null;
           mFrozenGame2 = new FrozenGame(mBackground, mBubbles, mBubblesBlind,
                                         mFrozenBubbles, mTargetedBubbles,
                                         mBubbleBlink, mGameWon, mGameLost,
@@ -1868,6 +1871,8 @@ public class GameView extends SurfaceView
           }
         }
 
+        mImagesReady = true;
+
         if (mHighScoreManager != null) {
           mHighScoreManager.startLevel(mLevelManager.getLevelIndex());
         }
@@ -1875,8 +1880,8 @@ public class GameView extends SurfaceView
     }
 
     private void nextLevel() {
+      mImagesReady = false;
       mPlayer1.setGameRef(null);
-      mFrozenGame1 = null;
       mFrozenGame1 = new FrozenGame(mBackground, mBubbles, mBubblesBlind,
                                     mFrozenBubbles, mTargetedBubbles,
                                     mBubbleBlink, mGameWon, mGameLost,
@@ -1885,6 +1890,7 @@ public class GameView extends SurfaceView
                                     mSoundManager, mLevelManager,
                                     mHighScoreManager);
       mPlayer1.setGameRef(mFrozenGame1);
+      mImagesReady = true;
       if (mHighScoreManager != null) {
         mHighScoreManager.startLevel(mLevelManager.getLevelIndex());
       }
@@ -1966,13 +1972,19 @@ public class GameView extends SurfaceView
     public void restoreState(Bundle map) {
       synchronized(mSurfaceHolder) {
         setState(stateEnum.PAUSED);
-        mFrozenGame1.restoreState(map, mImageList);
+        if (mFrozenGame1 != null) {
+          mFrozenGame1.restoreState(map, mImageList);
+        }
         if (numPlayers > 1) {
           numPlayer1GamesWon = map.getInt("numPlayer1GamesWon", 0);
           numPlayer2GamesWon = map.getInt("numPlayer2GamesWon", 0);
-          mFrozenGame2.restoreState(map, mImageList);
+          if (mFrozenGame2 != null) {
+            mFrozenGame2.restoreState(map, mImageList);
+          }
         }
-        mLevelManager.restoreState(map);
+        if (mLevelManager != null) {
+          mLevelManager.restoreState(map);
+        }
         if (mHighScoreManager != null) {
           mHighScoreManager.restoreState(map);
         }
@@ -2074,17 +2086,23 @@ public class GameView extends SurfaceView
     public Bundle saveState(Bundle map) {
       synchronized(mSurfaceHolder) {
         if (map != null) {
-          mFrozenGame1.saveState(map);
+          if (mFrozenGame1 != null) {
+            mFrozenGame1.saveState(map);
+          }
           if (numPlayers > 1) {
             map.putInt("numPlayers", 2);
             map.putInt("numPlayer1GamesWon", numPlayer1GamesWon);
             map.putInt("numPlayer2GamesWon", numPlayer2GamesWon);
-            mFrozenGame2.saveState(map);
+            if (mFrozenGame2 != null) {
+              mFrozenGame2.saveState(map);
+            }
           }
           else {
             map.putInt("numPlayers", 1);
           }
-          mLevelManager.saveState(map);
+          if (mLevelManager != null) {
+            mLevelManager.saveState(map);
+          }
           if (mHighScoreManager != null) {
             mHighScoreManager.saveState(map);
           }
